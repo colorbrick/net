5277871efc6dc970c198308cbd3d786b74c5b336
취업반PM <PM@itbank07-PC>
2015-06-04 18:47:09 +0900

    6/4..

--- src/net/socket/SwingChatClient.java
+++ src/net/socket/SwingChatClient.java
@@ -7,7 +7,10 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -37,16 +40,18 @@ public class SwingChatClient extends JFrame implements ActionListener, Runnable 
 	public SwingChatClient() {
 		super("채팅 : 클라이언트");
 		connectPane(); // 메소드는 동사를 사용하고 속성은 명사를 사용한다.
+		chatPane();
 		// card...
 		cardPane = new JPanel();
+		card = new CardLayout();
 		/*
 		 * LayoutManager 중에서 CardLayout 은
 		 * 탭과 비슷한 기능을 한다.
 		 * */
-		cardPane.setLayout(new CardLayout());
+		cardPane.setLayout(card);
 		cardPane.add(connectionPane, "접속창");
 		cardPane.add(chatPane,"채팅창");
-		cardPane.add(cardPane,"접속창");
+		card.show(cardPane,"접속창");
 		// ---------- card
 		add(cardPane);
 		setBounds(200,200,400,300);  // 위치, 사이즈
@@ -112,8 +117,24 @@ public class SwingChatClient extends JFrame implements ActionListener, Runnable 
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try {
+			socket = new Socket(ipTxt, PORT);
+			String nickName = txtName.getText();
+			os = socket.getOutputStream();
+			pw = new PrintWriter(new OutputStreamWriter(os));
+			pw.println(nickName);
+			pw.flush();
+			InputStream is = socket.getInputStream();
+			in = new BufferedReader(new InputStreamReader(is));
+			
+			String str="";
+			while (true) {
+				str = in.readLine();
+				txtList.append(str + "\n");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	@Override
